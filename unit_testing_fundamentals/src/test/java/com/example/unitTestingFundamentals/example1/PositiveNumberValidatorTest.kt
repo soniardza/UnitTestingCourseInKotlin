ae9ba4bc364b38180b5bc075378b3217a8f27650
“soniardza” <sonia.rdzalv@gmail.com>
2023-11-16 09:00:19 -0600

    [unitTestingFundamentals] first unit test

--- unit_testing_fundamentals/src/test/java/com/example/unitTestingFundamentals/example1/PositiveNumberValidatorTest.kt
+++ unit_testing_fundamentals/src/test/java/com/example/unitTestingFundamentals/example1/PositiveNumberValidatorTest.kt
@@ -1,3 +1,33 @@
 package com.example.unitTestingFundamentals.example1
 
-class PositiveNumberValidatorTest
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class PositiveNumberValidatorTest {
+    var SUT: PositiveNumberValidator? = null
+
+    @Before
+    fun setup() {
+        SUT = PositiveNumberValidator()
+    }
+
+    @Test
+    fun test1() {
+        val result = SUT!!.isPositive(-1)
+        Assert.assertThat(result, CoreMatchers.`is`(false))
+    }
+
+    @Test
+    fun test2() {
+        val result = SUT!!.isPositive(0)
+        Assert.assertThat(result, CoreMatchers.`is`(false))
+    }
+
+    @Test
+    fun test3() {
+        val result = SUT!!.isPositive(1)
+        Assert.assertThat(result, CoreMatchers.`is`(true))
+    }
+}
