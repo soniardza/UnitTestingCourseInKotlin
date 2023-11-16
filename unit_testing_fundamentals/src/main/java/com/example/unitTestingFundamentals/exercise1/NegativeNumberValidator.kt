01e9b4b0ae29338124a830e9af6d9113630d414c
“soniardza” <sonia.rdzalv@gmail.com>
2023-11-16 09:02:59 -0600

    [unitTestingFundamentals] Exercise 1

--- unit_testing_fundamentals/src/main/java/com/example/unitTestingFundamentals/exercise1/NegativeNumberValidator.kt
+++ unit_testing_fundamentals/src/main/java/com/example/unitTestingFundamentals/exercise1/NegativeNumberValidator.kt
@@ -3,6 +3,6 @@ package com.example.unitTestingFundamentals.exercise1
 class NegativeNumberValidator {
     fun isNegative(number: Int): Boolean {
         // the bug is that 0 will be reported as negative while it's not
-        return number <= 0
+        return number < 0
     }
 }
--- unit_testing_fundamentals/src/test/java/com/example/unitTestingFundamentals/exercise1/NegativeNumberValidatorTest.kt
+++ unit_testing_fundamentals/src/test/java/com/example/unitTestingFundamentals/exercise1/NegativeNumberValidatorTest.kt
@@ -1,3 +1,34 @@
 package com.example.unitTestingFundamentals.exercise1
 
-class NegativeNumberValidatorTest
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class NegativeNumberValidatorTest {
+
+    var SUT: NegativeNumberValidator? = null
+
+    @Before
+    fun setup() {
+        SUT = NegativeNumberValidator()
+    }
+
+    @Test
+    fun test1() {
+        val result = SUT!!.isNegative(-1)
+        Assert.assertThat(result, `is`(true))
+    }
+
+    @Test
+    fun test2() {
+        val result = SUT!!.isNegative(0)
+        Assert.assertThat(result, `is`(false))
+    }
+
+    @Test
+    fun test3() {
+        val result = SUT!!.isNegative(1)
+        Assert.assertThat(result, `is`(false))
+    }
+}
