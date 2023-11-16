bb26a3135460bb9bdb345060554ca9a13087e7d5
“soniardza” <sonia.rdzalv@gmail.com>
2023-11-16 09:06:00 -0600

    [unitTestingFundamentals] example 2

--- unit_testing_fundamentals/src/test/java/com/example/unitTestingFundamentals/example2/StringReverserTest.kt
+++ unit_testing_fundamentals/src/test/java/com/example/unitTestingFundamentals/example2/StringReverserTest.kt
@@ -1,3 +1,37 @@
 package com.example.unitTestingFundamentals.example2
 
-class StringReverserTest
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class StringReverserTest {
+    var SUT: StringReverser? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setup() {
+        SUT = StringReverser()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun reverse_emptyString_emptyStringReturned() {
+        val result = SUT!!.reverse("")
+        Assert.assertThat(result, CoreMatchers.`is`(""))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun reverse_singleCharacter_sameStringReturned() {
+        val result = SUT!!.reverse("a")
+        Assert.assertThat(result, CoreMatchers.`is`("a"))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun reverse_longString_reversedStringReturned() {
+        val result = SUT!!.reverse("Vasiliy Zukanov")
+        Assert.assertThat(result, CoreMatchers.`is`("vonakuZ yilisaV"))
+    }
+}
