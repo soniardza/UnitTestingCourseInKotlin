ce6faa0978ff0a0a817d65f4eff83e15b27d56cf
“soniardza” <sonia.rdzalv@gmail.com>
2024-04-17 16:34:53 -0600

    Update example 13

--- unit_testing_in_android/src/test/java/com/example/unitTestingInAndroid/example13/AndroidUnitTestingProblemsTest.kt
+++ unit_testing_in_android/src/test/java/com/example/unitTestingInAndroid/example13/AndroidUnitTestingProblemsTest.kt
@@ -1,5 +1,27 @@
 package com.example.unitTestingInAndroid.example13
 
-import org.junit.Assert.*
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 
-class AndroidUnitTestingProblemsTest
+@RunWith(MockitoJUnitRunner::class)
+class AndroidUnitTestingProblemsTest {
+
+    private lateinit var systemUnderTest: AndroidUnitTestingProblems
+
+    @Before
+    fun setup() {
+        systemUnderTest = AndroidUnitTestingProblems()
+    }
+
+    @Test
+    fun testStaticApiCall() {
+        // Arrange
+        // Act
+        systemUnderTest.callStaticAndroidApi("")
+        // Assert
+        assertTrue(true)
+    }
+}
